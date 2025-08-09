4d56494042c0687d55ebc259d542f98b01a4b54b
Your Name <your.email@example.com>
2025-08-09 17:40:05 +0530

    testing

--- src/library/axiosClient.ts
+++ src/library/axiosClient.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "http://localhost:6008/api/",
   headers: {
     "Content-Type": "application/json",
   },
